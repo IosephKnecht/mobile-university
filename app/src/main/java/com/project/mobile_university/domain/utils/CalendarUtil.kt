fc2c837c7de61e369643bf091f7c692abfbf4835
aa.mezencev <aa.mezencev@tensor.ru>
2019-01-29 16:39:35 +0300

    fix error calendar util calculate week

--- app/src/main/java/com/project/mobile_university/domain/utils/CalendarUtil.kt
+++ app/src/main/java/com/project/mobile_university/domain/utils/CalendarUtil.kt
@@ -5,6 +5,23 @@ import java.text.SimpleDateFormat
 import java.util.*
 
 object CalendarUtil {
+    private enum class DayEnum(private val calendarEnum: Int,
+                               val appEnum: Int) {
+        MONDAY(Calendar.MONDAY, 1),
+        TUESDAY(Calendar.TUESDAY, 2),
+        WEDNESDAY(Calendar.WEDNESDAY, 3),
+        THURSDAY(Calendar.THURSDAY, 4),
+        FRIDAY(Calendar.FRIDAY, 5),
+        SATURDAY(Calendar.SATURDAY, 6),
+        SUNDAY(Calendar.SUNDAY, 7);
+
+        companion object {
+            fun getAppEnumDay(value: Int): Int {
+                return values().find { it.calendarEnum == value }?.appEnum ?: 0
+            }
+        }
+    }
+
     @SuppressLint("ConstantLocale")
     private val simpleDateFormat = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
 
@@ -14,16 +31,17 @@ object CalendarUtil {
     }
 
     fun obtainMondayAndSunday(date: Date): Pair<Date, Date> {
-        val calendar = Calendar.getInstance()
-        calendar.firstDayOfWeek = Calendar.MONDAY
-        calendar.time = date
-        val i = calendar.get(Calendar.DAY_OF_WEEK) - calendar.firstDayOfWeek
-        calendar.add(Calendar.DATE, if (i != 0) -i - 7 else 0)
-        val start = calendar.time
-        calendar.add(Calendar.DATE, 6)
-        val end = calendar.time
+        val diff = obtainDayOfWeek(date) - DayEnum.MONDAY.appEnum
 
-        return Pair(start, end)
+        return Calendar.getInstance().apply {
+            time = date
+        }.run {
+            this.add(Calendar.DATE, -diff)
+            val start = this.time
+            this.add(Calendar.DATE, 6)
+            val end = this.time
+            Pair(start, end)
+        }
     }
 
     fun buildRangeBetweenDates(startWeek: Date, endWeek: Date): List<String> {
@@ -43,5 +61,11 @@ object CalendarUtil {
         }
 
         return datesInRange
+    }
+
+    private fun obtainDayOfWeek(date: Date): Int {
+        val calendar = Calendar.getInstance().apply { time = date }
+        val calendarEnum = calendar.get(Calendar.DAY_OF_WEEK)
+        return DayEnum.getAppEnumDay(calendarEnum)
     }
 }
