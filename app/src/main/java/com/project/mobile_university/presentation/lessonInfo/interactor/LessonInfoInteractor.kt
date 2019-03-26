db533e98bed4f2384826dfb811e9b14797d3597a
IosephKnecht <Andreich.mezya67@ya.ru>
2019-03-26 10:58:05 +0300

    added template for lesson info fragment

---
+++ app/src/main/java/com/project/mobile_university/presentation/lessonInfo/interactor/LessonInfoInteractor.kt
@@ -0,0 +1,8 @@
+package com.project.mobile_university.presentation.lessonInfo.interactor
+
+import com.project.iosephknecht.viper.interacor.AbstractInteractor
+import com.project.mobile_university.presentation.lessonInfo.contract.LessonInfoContract
+
+class LessonInfoInteractor : AbstractInteractor<LessonInfoContract.Listener>(),
+    LessonInfoContract.Interactor {
+}
---
+++ app/src/main/java/com/project/mobile_university/presentation/lessonInfo/presenter/LessonInfoPresenter.kt
@@ -0,0 +1,10 @@
+package com.project.mobile_university.presentation.lessonInfo.presenter
+
+import com.project.iosephknecht.viper.presenter.AbstractPresenter
+import com.project.mobile_university.presentation.lessonInfo.contract.LessonInfoContract
+
+class LessonInfoPresenter : AbstractPresenter(), LessonInfoContract.Presenter {
+    override fun onDestroy() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
